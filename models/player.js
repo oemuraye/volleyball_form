8bf54425f01b8b6bee07e30b65bca8b20c63ce4c
oemuraye <oemuraye@gmail.com>
2024-01-13 22:12:15 +0100

    init

---
+++ models/player.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const playerSchema = new mongoose.Schema({
+  id: {
+    type: String,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  profile_pic: {
+    type: String,
+    required: true,
+  },
+  date_of_birth: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  contact: {
+    type: Number,
+    required: true,
+  },
+  occupation: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model("Player", playerSchema);
